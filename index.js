ac74e7a7516293bf151d376fcd43c9b1d66f0872
Sulav Neupane <neupsulav@gmail.com>
2023-01-09 23:56:36 +0545

    end for today

--- index.js
+++ index.js
@@ -323,42 +323,41 @@
 //error in promises
 const loadScript = async (src) => {
   return new Promise((resolve, reject) => {
-    let info = notCheck(src);
-    if (!info) {
-      reject(new Error("script doesn't match"));
-    }
     let script = document.createElement("script");
     script.src = src;
     script.onload = () => {
       resolve("Loaded");
     };
+    if (!notCheck(src)) {
+      reject(new Error("script doesn't match"));
+    }
     document.body.append(script);
   });
 };
 
 const notCheck = (src) => {
   if (
-    (src =
-      "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js")
-  ) {
+    src ==
+    "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+  )
     return true;
-  } else {
+  else
     return false;
-  }
 };
 
 const mains = async () => {
-  let a = await loadScript(
-    "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-  );
-  console.log(a);
-  // try {
-  //   let a = await loadScript(
-  //     "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-  //   );
-  // } catch (e) {
-  //   console.log(e);
-  // }
+  // let a = await loadScript(
+  //   "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+  // );
+  // console.log(a);
+  try {
+    let a = await loadScript(
+      "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+    );
+    console.log(a);
+  } catch (e) {
+    console.log("This error has been handled");
+  }
 };
 
 mains();
